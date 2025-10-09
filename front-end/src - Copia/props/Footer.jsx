8dc71d06142325e43d81aa70a6371fcaed487143
Endrio <ruanito.mit@gmail.com>
2025-10-09 11:35:33 -0300

    ajustes footer

--- front-end/src - Copia/props/Footer.jsx
+++ front-end/src - Copia/props/Footer.jsx
@@ -3,29 +3,37 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer style={footerStyle}>
-      <div style={imagesContainerStyle}>
-        <img 
-          src="/assets/img/ifc.png" 
-          alt="Imagem 1" 
-          style={imageStyle} 
-        />
-        <img 
-          src="/assets/img/vitria.png" 
-          alt="Imagem 2" 
-          style={imageStyle} 
-        />
-        <img 
-          src="/assets/img/hackathon.png" 
-          alt="Imagem 3" 
-          style={imageStyle} 
-        />
+      <div style={logosWrapperStyle}>
+        <div style={logoItemStyle}>
+          <img 
+            src="/assets/img/ifc.png" 
+            alt="Logo IFC" 
+            style={ifcImageStyle} 
+          />
+        </div>
+        <div style={centralLogoStyle}>
+          <img 
+            src="/assets/img/vitria.png" 
+            alt="Logo Vitria" 
+            style={vitriaImageStyle} 
+          />
+        </div>
+        <div style={logoItemStyle}>
+          <img 
+            src="/assets/img/hackathon.png" 
+            alt="Logo Hackathon" 
+            style={hackathonImageStyle} 
+          />
+        </div>
       </div>
+
       <div style={emailContainerStyle}>
         <p>Entre em contato conosco pelo e-mail:</p>
         <a href="mailto:vitria.tech@gmail.com" style={emailLinkStyle}>
           vitria.tech@gmail.com
         </a>
       </div>
+
       <div style={copyrightContainerStyle}>
         <p style={copyrightStyle}>
           © Todos os direitos reservados Vitria Tech. Instituto Federal Catarinense Campus Concórdia
@@ -35,6 +43,7 @@ const Footer = () => {
   );
 };
 
+
 const footerStyle = {
   backgroundColor: '#ADBDD2',
   color: '#fff',
@@ -42,14 +51,49 @@ const footerStyle = {
   textAlign: 'center',
 };
 
-const imagesContainerStyle = {
+
+const logosWrapperStyle = {
   display: 'flex',
-  justifyContent: 'space-around',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  flexWrap: 'wrap',
+  gap: '30px', 
   marginBottom: '20px',
+  maxWidth: '800px',
+  marginLeft: 'auto',
+  marginRight: 'auto',
 };
 
-const imageStyle = {
-    height: '100px'
+const logoItemStyle = {
+  flex: '1 1 100px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+
+const centralLogoStyle = {
+  flex: '1 1 200px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+
+const ifcImageStyle = {
+  height: '60px',
+  maxWidth: '100%',
+};
+
+const vitriaImageStyle = {
+  height: '100px',
+  maxWidth: '100%',
+};
+
+const hackathonImageStyle = {
+  height: '80px',
+  maxWidth: '100%',
 };
 
 const emailContainerStyle = {
